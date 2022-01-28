bb8300f670bba9d0ec556e4f811f4f1d667aa33a
Siarhei Lesnikov <Sven2484@mail.ru>
2022-01-29 01:58:37 +0300

    Fine

--- tasks/variables/n_nn_nnn.py
+++ tasks/variables/n_nn_nnn.py
@@ -19,10 +19,10 @@ def n_sum(n: int) -> int:
 
     :return: результат выполнения
     """
-    result = None
+    result = n + n ** 2 + n ** 3
     return result
 
 
 if __name__ == '__main__':
     n_number = int(input('Введите число n: '))
-    print(f'Результат n+nn+nnn: {n_sum(n_number)}')
+    print(f'Результат: {n_sum(n_number)}')
